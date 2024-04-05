3f2a00daae54740448aff6641d78920930a358bd
Balu <baluproton+gitlab@protonmail.com>
2024-04-05 12:29:17 +0200

    Routing layout
    Co-authored-by: Sara

--- main.go
+++ main.go
@@ -3,11 +3,36 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func main() {
+
+	// Create a new instance of the Fiber app.
 	app := fiber.New()
 
+	// Routes
 	app.Get("/ping", func(c *fiber.Ctx) error {
 		return c.SendString("Pong!")
 	})
 
+	// Routes for the puzzles
+	app.Get("/puzzles", func(c *fiber.Ctx) error {
+		return c.SendString("Unimplemented!")
+	})
+
+	app.Get("/puzzles/:id", func(c *fiber.Ctx) error {
+		return c.SendString("Unimplemented!")
+	})
+
+	app.Post("/puzzles", func(c *fiber.Ctx) error {
+		return c.SendString("Unimplemented!")
+	})
+
+	app.Patch("/puzzles", func(c *fiber.Ctx) error {
+		return c.SendString("Unimplemented!")
+	})
+
+	app.Delete("/puzzles", func(c *fiber.Ctx) error {
+		return c.SendString("Unimplemented!")
+	})
+
+	// Start the server and listen on port 3000
 	app.Listen(":3000")
 }
