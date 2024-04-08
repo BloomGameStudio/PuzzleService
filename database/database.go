42c94abf8a3d3d8a99968fd09091a532273b1b73
sarahtonein <joemoe1899@gmail.com>
2024-04-07 23:51:30 -0400

    DBInit & AutoMigrate

---
+++ database/database.go
@@ -0,0 +1,15 @@
+package database
+
+import (
+	"gorm.io/gorm"
+)
+
+func DBInit() (*gorm.DB, error) {
+    //db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+    //if err != nil {
+    //    return nil, err
+    //}
+    //return db, nil
+
+	return nil, nil
+}
---
+++ database/init.go
@@ -0,0 +1,10 @@
+package database
+
+import (
+	"gorm.io/gorm"
+)
+
+func AutoMigrate(db *gorm.DB) error {
+    // Not implemented
+	return nil
+}
