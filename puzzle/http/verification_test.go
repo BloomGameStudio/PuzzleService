c1b04784fc64764901b1c994b930b47c9d8db7bd
sarahtonein <joemoe1899@gmail.com>
2024-05-07 23:12:35 -0400

    Test comments & valid test

--- puzzle/http/verification_test.go
+++ puzzle/http/verification_test.go
@@ -9,6 +9,27 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// Test verify route using mock solution
+func TestVerifySolutionHandlerWithCorrectSolution(t *testing.T){
+    app := fiber.New()
+    app.Post("/verify", VerifySolutionHandler)
+
+    body := bytes.NewBufferString(`{"ID":1,"Data":"Correct!"}`)
+    req := httptest.NewRequest("POST", "/verify", body)
+    req.Header.Set("Content-Type", "application/json")
+
+    resp, err := app.Test(req, -1)
+    if err != nil {
+        t.Fatalf("Failed to send request: %s", err)
+    
+    }
+
+    if resp.StatusCode != http.StatusOK {
+        t.Errorf("Expected status OK, got %s", resp.Status)
+    }
+}
+
+// Test verify route using incorrect solution for a given ID
 func TestVerifySolutionHandlerWithIncorrectSolution(t *testing.T) {
     app := fiber.New()
     app.Post("/verify", VerifySolutionHandler)
@@ -27,6 +48,7 @@ func TestVerifySolutionHandlerWithIncorrectSolution(t *testing.T) {
     }
 }
 
+// Test verify route using invalid request
 func TestVerifySolutionHandlerWithInvalidRequest(t *testing.T) {
     app := fiber.New()
     app.Post("/verify", VerifySolutionHandler)
@@ -45,6 +67,7 @@ func TestVerifySolutionHandlerWithInvalidRequest(t *testing.T) {
     }
 }
 
+// Test verify route using no solution found
 func TestVerifySolutionHandlerWithNoSolutionFound(t *testing.T) {
     app := fiber.New()
     app.Post("/verify", VerifySolutionHandler)
